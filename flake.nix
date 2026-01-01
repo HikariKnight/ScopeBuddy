51cbc9a9daef05ff8015dc848f55cb1e0828a2ab
david <david@david.rip>
2026-01-01 07:22:38 -0500

    build: add NixOS support (#34)
    * added flake.nix
    
    * Added NixOS Support
    
    * fixed typo
    
    * readme formatting

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "ScopeBuddy – gamescope wrapper for Wayland";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    systems = [ "x86_64-linux" "aarch64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs systems;
+  in
+  {
+    packages = forAllSystems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        default = pkgs.stdenvNoCC.mkDerivation {
+          pname = "scopebuddy";
+          version = "git";
+          src = self;
+
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+
+          installPhase = ''
+            install -Dm755 bin/scopebuddy $out/bin/scopebuddy
+            ln -s $out/bin/scopebuddy $out/bin/scb
+
+            wrapProgram $out/bin/scopebuddy \
+              --prefix PATH : ${pkgs.lib.makeBinPath [
+                pkgs.gamescope
+                pkgs.perl
+                pkgs.jq
+                pkgs.wlr-randr
+              ]}
+          '';
+        };
+      }
+    );
+
+    apps = forAllSystems (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/scopebuddy";
+      };
+    });
+  };
+}
